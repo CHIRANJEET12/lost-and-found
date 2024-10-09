d21280a97ae990a42047489e3f0df145f1ffe2a5
CHIRANJEET DWIVEDY <154692196+CHIRANJEET12@users.noreply.github.com>
2024-10-09 12:14:26 +0530

    Update Foundd.jsx

--- client/src/components1/Foundd.jsx
+++ client/src/components1/Foundd.jsx
@@ -26,7 +26,7 @@ const FoundItemForm = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/found`, {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}found`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
